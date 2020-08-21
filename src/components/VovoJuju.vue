54e6ddf6d9250e2c5d807cb4e7693599c1860fd8
Raphael Matori <raphaelmatori@live.com>
2020-08-21 18:42:08 -0300

    Fix - Mantem centralizado ao alterar tamanho da tela

--- src/components/VovoJuju.vue
+++ src/components/VovoJuju.vue
@@ -79,7 +79,9 @@ export default {
     position relative
     z-index 100
     padding 1.25rem /* 20/16 */
+    width initial
     @media screen and (max-width: 768px)
+      width 100%
       text-align center
   &__header
     margin 1.875rem /* 30/16 */ 0
